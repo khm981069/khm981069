19e4330adbd7c61a2b453ced946547243865ba73
khm981069 <81234094+khm981069@users.noreply.github.com>
2022-03-15 00:16:49 +0800

    Update playlist2.js

--- scripts/playlist2.js
+++ scripts/playlist2.js
@@ -1,12 +1,10 @@
 trackList = 
 
 [
-{name:"放鬆音樂1",artist:"1 ",src:"music/放鬆音樂1.mp3"},
+
 {name:"放鬆音樂2",artist:"1 ",src:"music/放鬆音樂2.mp3"},
-{name:"放鬆音樂3",artist:"1 ",src:"music/放鬆音樂3.mp3"},
-  
-{name:"學習專注力讀書音樂1",artist:"1 ",src:"music/學習專注力讀書音樂1.mp3"},
+
 {name:"學習專注力讀書音樂2",artist:"1 ",src:"music/學習專注力讀書音樂2.mp3"},
-{name:"學習專注力讀書音樂3",artist:"1 ",src:"music/學習專注力讀書音樂3.mp3"},
+
 {name:"瘦子E.SO 妹妹（歌詞）",artist:"瘦子E.SO ",src:"music/瘦子E.SO 妹妹（歌詞）.mp3"}
 ];
