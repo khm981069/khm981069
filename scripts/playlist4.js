55c08c4217686321601fee830f880ef30df7bfcb
khm981069 <81234094+khm981069@users.noreply.github.com>
2022-03-14 21:48:35 +0800

    Update playlist4.js

--- scripts/playlist4.js
+++ scripts/playlist4.js
@@ -1,4 +1,6 @@
 trackList = 
 [
 {name:"瘦子E.SO 妹妹（歌詞）",artist:"瘦子E.SO ",src:"music/瘦子E.SO 妹妹（歌詞）.mp3"},
+{name:"瘦子E.SO 妹妹（歌詞）",artist:"瘦子E.SO ",src:"music/瘦子E.SO 妹妹（歌詞）.mp3"},
+{name:"瘦子E.SO 妹妹（歌詞）",artist:"瘦子E.SO ",src:"music/瘦子E.SO 妹妹（歌詞）.mp3"}
 ];
