ab3e229a11872ae1446c3418a5b476124a20142b
khm981069 <81234094+khm981069@users.noreply.github.com>
2022-03-15 00:20:45 +0800

    Update playlist4.js

--- scripts/playlist4.js
+++ scripts/playlist4.js
@@ -1,6 +1,20 @@
 trackList = 
 [
-{name:"瘦子E.SO 妹妹（歌詞）",artist:"瘦子E.SO ",src:"music/瘦子E.SO 妹妹（歌詞）.mp3"},
-{name:"瘦子E.SO 妹妹（歌詞）",artist:"瘦子E.SO ",src:"music/瘦子E.SO 妹妹（歌詞）.mp3"},
+{name:"陳忻玥 Vicky Chen -【You Like Me】Official MV",artist:"陳忻玥 ",src:"music/陳忻玥 Vicky Chen -【You Like Me】Official MV.mp3"},
+{name:"0064. 九九Sophie Chen陳忻玥Vicky Chen-【Fall in love】歌詞Lyrics",artist:"陳忻玥 ",src:"music/九九Sophie Chen陳忻玥Vicky Chen-【Fall in love】歌詞Lyrics.mp3"},
+
+{name:"0201. 陳忻玥 Am I Who I Am 歌詞",artist:"陳忻玥 ",src:"music/陳忻玥 Am I Who I Am 歌詞.mp3"},
+{name:"0202. 陳忻玥 社會敗類 歌詞",artist:"陳忻玥 ",src:"music/陳忻玥 社會敗類 歌詞.mp3"},
+{name:"0108. 陳忻玥 Vicky Chen - 煙幕 (Smokescreen)",artist:"陳忻玥 ",src:"music/陳忻玥 Vicky Chen - 煙幕 (Smokescreen).mp3"},
+{name:"0109. 陳忻玥 Vicky Chen -【Hands Up】Official MV",artist:"陳忻玥 ",src:"music/陳忻玥 Vicky Chen -【Hands Up】Official MV.mp3"},
+{name:"0110. 陳忻玥 Vicky Chen -【Treasure】Official MV",artist:"陳忻玥 ",src:"music/陳忻玥 Vicky Chen -【Treasure】Official MV.mp3"},
+{name:"0111. 陳忻玥 Vicky Chen -【入土之前 Till Im Underground】Official MV",artist:"陳忻玥 ",src:"music/陳忻玥 Vicky Chen -【入土之前 Till Im Underground】Official MV.mp3"},
+{name:"0112. 陳忻玥 Vicky Chen -【炙愛 (女聲版) Fiery Love Vicky Version】Official MV",artist:"陳忻玥 ",src:"music/陳忻玥 Vicky Chen -【炙愛 (女聲版) Fiery Love Vicky Version】Official MV.mp3"},
+{name:"0113. 陳忻玥-My Love is Selfish 歌詞",artist:"陳忻玥 ",src:"music/陳忻玥-My Love is Selfish 歌詞.mp3"},
+{name:"0079. 我命不由天",artist:"無 ",src:"music/我命不由天.mp3"},
+{name:"0047. Bad Guy壞女人 ",artist:"Billie Eilish ",src:"music/Billie Eilish - Bad Guy壞女人中文字幕 Lyrics.mp3"},  
+{name:"0085. 汪蘇瀧 - 一笑傾城《微微一笑很傾城片頭曲》【動態歌詞Lyrics】",artist:"汪蘇瀧 ",src:"music/汪蘇瀧 - 一笑傾城《微微一笑很傾城片頭曲》【動態歌詞Lyrics】.mp3"},
+{name:"0084. 熱狗 feat.艾怡良 怨偶",artist:"艾怡良 ",src:"music/熱狗 feat.艾怡良 怨偶.mp3"},
+
 {name:"瘦子E.SO 妹妹（歌詞）",artist:"瘦子E.SO ",src:"music/瘦子E.SO 妹妹（歌詞）.mp3"}
 ];
