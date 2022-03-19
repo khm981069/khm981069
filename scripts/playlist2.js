ca892a697d16ae68b5f0a643e0055a9f0f3ff419
khm981069 <81234094+khm981069@users.noreply.github.com>
2022-03-19 09:28:04 +0800

    Update playlist2.js

--- scripts/playlist2.js
+++ scripts/playlist2.js
@@ -5,6 +5,13 @@ trackList =
 {name:"放鬆音樂2",artist:"1 ",src:"music/放鬆音樂2.mp3"},
 
 {name:"學習專注力讀書音樂2",artist:"1 ",src:"music/學習專注力讀書音樂2.mp3"},
-
+{name:"電子音樂1",artist:"1 ",src:"music/電子音樂1.mp3"},
+{name:"電子音樂2",artist:"1 ",src:"music/電子音樂2.mp3"},
+  {name:"電子音樂3",artist:"1 ",src:"music/電子音樂3.mp3"},
+  {name:"電子音樂4",artist:"1 ",src:"music/電子音樂4.mp3"},
+  {name:"電子音樂5",artist:"1 ",src:"music/電子音樂5.mp3"},
+  {name:"電子音樂6",artist:"1 ",src:"music/電子音樂6.mp3"},
+  {name:"電子音樂7",artist:"1 ",src:"music/電子音樂7.mp3"},
+  
 {name:"瘦子E.SO 妹妹（歌詞）",artist:"瘦子E.SO ",src:"music/瘦子E.SO 妹妹（歌詞）.mp3"}
 ];
