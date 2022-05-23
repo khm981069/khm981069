d1fadc1807c9eb8c0a44b5537ac7a8d48963ff70
khm981069 <81234094+khm981069@users.noreply.github.com>
2022-05-23 21:29:26 +0800

    Update playlist5.js

--- scripts/playlist5.js
+++ scripts/playlist5.js
@@ -1,5 +1,27 @@
 trackList = 
 [
-{name:"放鬆音樂2",artist:"1 ",src:"music/放鬆音樂2.mp3"},
-{name:"050. 瘦子E.SO - CHANGE",artist:"NA",src:"20210507_20210513/050. 瘦子E.SO - CHANGE.mp3"}
+{name:"LiSA - 紅蓮華",artist:"lisa ",src:"music/LiSA - 紅蓮華.mp3"},
+{name:"灌篮高手-好想大声说爱你",artist:"灌篮高手-好想大声说爱你 ",src:"music/灌篮高手-好想大声说爱你.mp3"},
+
+{name:"七龍珠-原始版",artist:"七龍珠 ",src:"music/七龍珠-原始版.mp3"},
+{name:"千本櫻",artist:"千本櫻 ",src:"music/千本櫻.mp3"},
+{name:"火影忍者-GO",artist:"火影忍者-GO ",src:"music/火影忍者-GO.mp3"},
+{name:"火影忍者-青鳥",artist:"火影忍者-青鳥 ",src:"music/火影忍者-青鳥.mp3"},
+{name:"你的名字-前前前世",artist:"你的名字-前前前世 ",src:"music/你的名字-前前前世.mp3"},
+{name:"呪術廻戦-廻廻奇譚",artist:"呪術廻戦-廻廻奇譚 ",src:"music/呪術廻戦-廻廻奇譚.mp3"},
+{name:"神奇寶貝-OK",artist:"神奇寶貝-OK ",src:"music/神奇寶貝-OK.mp3"},
+{name:"寄生獸- Let Me Hear",artist:"寄生獸- Let Me Hear ",src:"music/寄生獸- Let Me Hear.mp3"},
+{name:"進擊- 紅蓮弓矢",artist:"進擊- 紅蓮弓矢 ",src:"music/進擊- 紅蓮弓矢.mp3"},
+{name:"新世紀福音戰士-残酷天使",artist:"新世紀福音戰士-残酷天使 ",src:"music/新世紀福音戰士-残酷天使.mp3"},
+{name:"極楽浄土",artist:"極楽浄土 ",src:"music/極楽浄土.mp3"},
+{name:"數碼寶貝-Brave Heart",artist:"數碼寶貝-Brave Heart ",src:"music/數碼寶貝-Brave Heart.mp3"}, 
+{name:"數碼寶貝-Butterfly",artist:"數碼寶貝-Butterfly ",src:"music/數碼寶貝-Butterfly.mp3"},
+{name:"龍珠-漸漸被你吸引",artist:"龍珠-漸漸被你吸引",src:"music/龍珠-漸漸被你吸引.mp3"},
+{name:"獵人-早安",artist:"獵人-早安",src:"music/獵人-早安.mp3"},
+{name:"獵人-貪婪之島",artist:"獵人-貪婪之島",src:"music/獵人-貪婪之島.mp3"},
+{name:"寶可夢大師",artist:"寶可夢大師 ",src:"music/寶可夢大師.mp3"},
+{name:"一拳超人-THE HERO",artist:"/一拳超人",src:"music/一拳超人-THE HERO.mp3"}
+  
+  
+
 ]
